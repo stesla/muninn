8e39b8b9a0c78dc17f47c160f2a20a3a21b29294
Samuel Tesla <samuel.tesla@gmail.com>
2023-10-02 20:40:33 -0500

    get nextjs frontend stubbed out

--- main.go
+++ main.go
@@ -2,26 +2,26 @@ package main
 
 import (
 	"embed"
+	"encoding/json"
 	"io/fs"
 	"net/http"
 	"strings"
 
-	"github.com/gin-gonic/gin"
+	"github.com/julienschmidt/httprouter"
 	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
 	"github.com/spf13/pflag"
 	"github.com/spf13/viper"
 )
 
-//go:embed all:nextjs/out
+//go:embed all:nextjs/dist
 var nextFS embed.FS
 
 func main() {
 	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
 
-	pflag.String("address", ":8080", "address to which we should bind")
+	pflag.String("address", ":3001", "address to which we should bind")
 	viper.BindPFlag("http.address", pflag.Lookup("address"))
-	viper.SetDefault("http.address", ":8080")
 
 	config := pflag.String("config", "", "path to config file")
 
@@ -45,53 +45,24 @@ func main() {
 		panic(err)
 	}
 
-	r := gin.New()
-	r.SetTrustedProxies(nil)
-
-	r.Use(
-		gin.Recovery(),
-		Logger(),
-	)
-
-	outFS, _ := fs.Sub(nextFS, "nextjs/out")
-	r.StaticFS("/", http.FS(outFS))
-	r.NoRoute(func(c *gin.Context) {
-		c.FileFromFS("404.html", http.FS(outFS))
-	})
-
-	addr := viper.GetString("http.address")
-	log.Info().
-		Str("http.address", addr).
-		Msg("starting gin server")
-	r.Run(viper.GetString("http.address"))
-}
-
-func Logger() gin.HandlerFunc {
-	return func(c *gin.Context) {
-		path := c.Request.URL.Path
-		rawQuery := c.Request.URL.RawQuery
-		if rawQuery != "" {
-			path = path + "?" + rawQuery
-		}
-
-		c.Next()
-
-		var event *zerolog.Event
-		statusCode := c.Writer.Status()
-		switch {
-		case statusCode >= 500:
-			event = log.Error()
-		case statusCode >= 400 && statusCode < 500:
-			event = log.Warn()
-		default:
-			event = log.Info()
-		}
-		event.
-			Str("client_ip", c.ClientIP()).
-			Str("method", c.Request.Method).
-			Str("path", path).
-			Int("http_status", statusCode).
-			Strs("errors", c.Errors.Errors()).
-			Msg("")
+	distFS, err := fs.Sub(nextFS, "nextjs/dist")
+	if err != nil {
+		panic(err)
 	}
+
+	api := httprouter.New()
+	api.GET("/ping", httprouter.Handle(func(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+		pong := map[string]string{"ping": "pong"}
+		json.NewEncoder(w).Encode(&pong)
+	}))
+
+	http.Handle("/", http.FileServer(http.FS(distFS)))
+	http.Handle("/api/", http.StripPrefix("/api", api))
+
+	log.Info().
+		Str("http.address", viper.GetString("http.address")).
+		Msg("started")
+
+	err = http.ListenAndServe(viper.GetString("http.address"), nil)
+	log.Fatal().Err(err).Msg("")
 }
--- nextjs/next.config.js
+++ nextjs/next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
+    output: process.env.EXPORT ? 'export' : undefined,
+    distDir: process.env.EXPORT ? 'dist' : undefined,
+
+    rewrites: (!process.env.EXPORT) ? async function() {
+        return [
+            {
+                source: "/api/:path*",
+                destination: "http://localhost:3001/api/:path*",
+            }
+        ]
+    } : undefined,
 }
 
 module.exports = nextConfig
