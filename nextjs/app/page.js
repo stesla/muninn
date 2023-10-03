2781e9a86795374c9b31b8fdeb3cab9eb8bb3cf9
Samuel Tesla <samuel.tesla@gmail.com>
2023-10-03 18:44:27 -0500

    use next/image

--- nextjs/app/page.js
+++ nextjs/app/page.js
@@ -1,9 +1,11 @@
+import Image from 'next/image'
+
 export default function Home() {
   return (
     <main className="min-h-screen p-24">
       <div className="flex flex-col items-center">
         <h1 className="text-9xl bolder">Muninn</h1>
-        <img
+        <Image
           src="/img/munin-logo.png"
           alt="Munin Logo"
           width={480}
